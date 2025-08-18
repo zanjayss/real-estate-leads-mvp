7713a78b48762960c07d234bc91660c975171e54
Zanja <zanjayssel@icloud.com>
2025-08-18 15:49:25 +0200

    Fix google.auth.JWT constructor for googleapis v105+

--- src/lib/sheets.ts
+++ src/lib/sheets.ts
@@ -5,12 +5,12 @@ const credentials = JSON.parse(
   Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64!, "base64").toString("utf8")
 );
 
-const auth = new google.auth.JWT(
-  credentials.client_email,
-  undefined,
-  credentials.private_key,
-  ["https://www.googleapis.com/auth/spreadsheets"]
-);
+const auth = new google.auth.JWT({
+  email: credentials.client_email,
+  key: credentials.private_key,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
 
 const sheets = google.sheets({ version: "v4", auth });
 
